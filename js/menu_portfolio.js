57c7ecac3f6b21fc3f19906bbb91c115397e965b
ulihtenshtein <ulihtenshtein@gmail.com>
2017-08-11 17:31:04 +0700

    промсох

--- js/menu_portfolio.js
+++ js/menu_portfolio.js
@@ -23,10 +23,10 @@ function MenuPortfolioForm(options)
 	elem.on('click','#menu-portfolio-submit_btn', onSave);
 	elem.on('click','#menu-portfolio-close_btn',onClose);
 	elem.on('input','#menu-portfolio-newName-input',onChangeName);
-	elem.on('click','#menu-portfolio-size-fix',onSizeFix);
-	elem.on('click','#menu-portfolio-crop-fix',onCropFix);
-	elem.on('input','#menu-portfolio-size-width', onSizeWidth);
-	elem.on('input','#menu-portfolio-size-height', onSizeHeight);
+	// elem.on('click','#menu-portfolio-size-fix',onSizeFix);
+	// elem.on('click','#menu-portfolio-crop-fix',onCropFix);
+	elem.on('input','#menu-portfolio-dest-width', onDestWidth);
+	// elem.on('input','#menu-portfolio-dest-height', onDestHeight);
 	// elem.find('#menu-portfolio-crop-width').on('input',onCropWidth);
 	// elem.on('click','#menu-portfolio-crop-width', onCropWidth);
 	// elem.on('input','#menu-portfolio-crop-height', onCropHeight);
@@ -85,58 +85,38 @@ function MenuPortfolioForm(options)
 		span.classList.toggle('menu-portfolio-black');
 		event.preventDefault();
 	}
-	function onSizeWidth(event)
+	function onDestWidth(event)
 	{
-		var aspectRatio = source_w / source_h;
-		var value = event.target.value;
-		if (sizeFix) 
-		{
-			var sizeHight = elem.find('#menu-portfolio-size-height');
-			var valueHeight = Math.floor(value / aspectRatio);
-			sizeHight.val(valueHeight);
-			TARGET_H = valueHeight;
-		}
+		var value = parseInt(event.target.value);
 		TARGET_W = value;
+		elem.find('#target_w').val(value);
+		createCrop();
 	}
-	function onSizeHeight(event)
+	function onDestHeight(event)
 	{
-		var aspectRatio = source_w / source_h;
-		var value = event.target.value;
-		if (sizeFix) 
-		{
-			var sizeWidth = elem.find('#menu-portfolio-size-width');
-			var valueWidth = Math.floor(value * aspectRatio);
-			sizeWidth.val(valueWidth);
-			TARGET_W = valueWidth;
-		}	
+		var value = parseInt(event.target.value);	
 		TARGET_H = value;
+		elem.find('#target_h').val(value);
+		self.jcrop_api.setOptions({aspectRatio:TARGET_W/TARGET_H});
 	}
 	function onCropWidth(event)
 	{
+
 		var value = parseInt(event.target.value);
-		var x = parseInt(elem.find('#x').val());
-		var y = parseInt(elem.find('#y').val());
-		var h = parseInt(elem.find('#h').val());
-
-		elem.find('#w').val(value);	
-
-		if (cropFix) 
+		if ( value > 0 ) 
 		{
-			var cropHight = elem.find('#menu-portfolio-crop-height');
-			cropHight.val(value);
-			elem.find('#h').val(value);
-			if (self.jcrop_api)
-			{
-				self.jcrop_api.setSelect([x,y,value,value]);
-			}
+			var x = parseInt(elem.find('#x').val());
+			var y = parseInt(elem.find('#y').val());
+
+			var aspectRatio = TARGET_W / TARGET_H;
+			var h = parseInt( value / aspectRatio );
+
+			elem.find('#w').val(value);	
+			elem.find('#h').val(h);
+
+			self.jcrop_api.setOptions({setSelect:[x,y,x+value,y+h]});
 		}
-		else
-		{
-			if (self.jcrop_api)
-			{
-				self.jcrop_api.setSelect([x,y,value,h]);
-			}
-		}
+		
 	}
 
 	function onCropHeight(event)
@@ -219,37 +199,47 @@ function MenuPortfolioForm(options)
 					source_h = this.height;
 					elem.find('#source_h').val(source_h);
 					elem.find('#source_w').val(source_w);
-					elem.find('#menu-portfolio-size-width').val(source_w);
-					elem.find('#menu-portfolio-size-height').val(source_h);
+					elem.find('#menu-portfolio-size-width').html(source_w);
+					elem.find('#menu-portfolio-size-height').html(source_h);
 					elem.find('#menu-portfolio-crop-width').val(TARGET_W)
 					elem.find('#menu-portfolio-crop-height').val(TARGET_H);
-
-					try 
-					{
-						self.jcrop_api.destroy();
-					} 
-					catch (e) 
-					{
-							// object not defined
-					}
-					var aspectRatio = source_w / source_h;( cropFix ) ? 1 : 0;//source_w / source_h
-					$('#menu-portfolio-img > img').Jcrop({
-				      // trueSize:[source_w,source_h],
-				      // aspectRatio: aspectRatio,
-				      boxWidth:sizeImage,
-				      boxHeight:sizeImage,
-				      setSelect: [ 100, 100, TARGET_W, TARGET_H ],
-				      onSelect: updateCoords,
-				      onChange: updateCoords
-				    },function(){
-				        self.jcrop_api = this;
-				    });
+					createCrop();	
+					
 				};
 			};
 		}
 	}
 	//end loadFile
-	
+	function createCrop()
+	{
+		try 
+		{
+			self.jcrop_api.destroy();
+			self.jcrop_api = null;
+		} 
+		catch (e) 
+		{
+				// object not defined
+		}
+		var aspectRatio = TARGET_W / TARGET_H;
+		
+		var x = 100;//resizeCoords.getBoxX();
+		var y = 100;//resizeCoords.getBoxY();
+		var w = 200;//resizeCoords.getBoxW();
+		var h = 200;//resizeCoords.getBoxH();
+
+		$('#menu-portfolio-img > img').Jcrop({
+	      trueSize:[source_w,source_h],
+	      aspectRatio: aspectRatio,
+	      boxWidth:sizeImage,
+	      boxHeight:sizeImage,
+	      setSelect: [ x, y, x + w , y + h ],
+	      onSelect: updateCoords,
+	      onChange: updateCoords
+	    },function(){
+	        self.jcrop_api = this;
+	    });
+	}
 	function setNewName(newName)
 	{
 		var name = elem.find('.menu-portfolio-newName-result')
@@ -266,31 +256,29 @@ function MenuPortfolioForm(options)
 	// updateCoords : updates hidden input values after every crop selection
 	function updateCoords(c)
 	{
-		var x = parseInt(c.x * source_w / sizeImage);
-		var y = parseInt(c.y * source_h / sizeImage);
-
+		var resizeCoords = new ResizeCoords({
+			x:c.x,
+			y:c.y,
+			w:c.w,
+			h:c.h,
+			source_w:source_w,
+			source_h:source_h,
+			sizeImage:sizeImage
+		});
 		var aspectRatio = source_w / source_h;
-		var width = c.w / sizeImage * source_w ;
-		var height = c.h / sizeImage * source_h ;
 
-		width = (aspectRatio > 1) ? parseInt(width) : parseInt(width / aspectRatio);
-		height = (aspectRatio < 1) ? parseInt(height) : parseInt(height * aspectRatio);
-		x = (aspectRatio > 1) ? parseInt(x) : parseInt(x / aspectRatio);
-		y = (aspectRatio < 1) ? parseInt(y) : parseInt(y * aspectRatio);
+		var x = c.x;//resizeCoords.getSrcX();
+		var y = c.y;//resizeCoords.getSrcY();
+
+		var width = parseInt(c.w);//resizeCoords.getSrcW();
+		var height = parseInt(c.h * aspectRatio);//resizeCoords.getSrcH();
 
 		elem.find('#x').val(x);
 		elem.find('#y').val(y);
 		elem.find('#w').val(width);
 		elem.find('#h').val(height);
 		elem.find('#menu-portfolio-crop-width').val(width);
-		if (cropFix )
-		{
-			elem.find('#menu-portfolio-crop-height').val(width);
-		}
-		else
-		{
-			elem.find('#menu-portfolio-crop-height').val(height);
-		}
+		elem.find('#menu-portfolio-crop-height').val(height);
 	}
 
 	function clear()
@@ -332,5 +320,71 @@ function MenuPortfolioForm(options)
 }
 //end class MenuPortfolioForm
 
+// class resizeCoords
+function ResizeCoords(opts)
+{
+	var source_w = opts.source_w;
+	var source_h = opts.source_h;
+	var sizeImage = opts.sizeImage;
+	var x = opts.x;
+	var y = opts.y;
+	var w = opts.w;
+	var h = opts.h;
+	var aspectRatio = source_w / source_h;
+
+	function axisSrcX(x)
+	{
+		var src_x = x * source_w / sizeImage;
+		return (aspectRatio > 1) ? parseInt(src_x) : parseInt(src_x / aspectRatio);
+	}
+	function axisSrcY(y)
+	{
+		var src_y = y * source_h / sizeImage;
+		return (aspectRatio < 1) ? parseInt(src_y) : parseInt(src_y * aspectRatio);	
+	}
+	function axisBoxX(x)
+	{
+		var box_x = x * sizeImage / source_w ;
+		return (aspectRatio > 1) ? parseInt(box_x) : parseInt(box_x * aspectRatio);
+	}
+	function axisBoxY(y)
+	{
+		var box_y = y * sizeImage / source_h ;
+		return (aspectRatio < 1) ? parseInt(box_y) : parseInt(box_y  / aspectRatio);	
+	}
+
+	this.getSrcX = function() 
+	{
+		return axisSrcX(x);
+	}
+	this.getSrcY = function()
+	{
+		return axisSrcY(y);
+	}
+	this.getSrcW = function()
+	{
+		return axisSrcX(w);
+	}
+	this.getSrcH = function()
+	{
+		return axisSrcY(h);
+	}
+	this.getBoxX = function()
+	{
+		return axisBoxX(x);
+	}
+	this.getBoxY = function()
+	{
+		return axisBoxY(y);
+	}
+	this.getBoxW = function()
+	{
+		return axisBoxX(w);
+	}
+	this.getBoxH = function()
+	{
+		return axisBoxY(h);
+	}
+}// end class ResizeCoords
 var menu_portfolio_form = new MenuPortfolioForm({ elem:$('#popup-menu-portfolio'),open_btn:$('#open_btn')});
 	

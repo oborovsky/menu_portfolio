c69013f50c7baa9ec110893eecb814815e5182d6
doborovsky <doborovsky@gmail.com>
2017-08-16 02:36:52 +0700

    доделал всплывающее окно подсказки

--- help_info.php
+++ help_info.php
@@ -1,28 +1,54 @@
 <?php
+$user = "root";
+$password = "4995";
 $host='localhot';
-$db = 'help_info';
-$table = 'help_info';
+$database = 'test';
+$table = 'help_tbl';
 $type = $_POST['type'];
+$idHelpInfo = $_POST['id'];
 
-$helpText='gagaga blablabala utiputi !';
+$bd = mysql_connect($hostname, $user, $password) or die("Opps some thing went wrong");
+mysql_select_db($database, $bd) or die("Opps some thing went wrong");
+
 if ( $type == 'get')
 {
-	$idHelpInfo = $_POST['id'];
+	$helpText='error';
+	$strQuery = "select help from help_tbl where id=".$idHelpInfo;
+	$result = mysql_query($strQuery);
+	if( !$result)
+	{
+		die("невеный запрос:".mysql_error());
+	}
+	$row = mysql_fetch_assoc($result);
+	if ( !$row )
+	{
+		die("нет данных:".mysql_error());
+	}
+	$helpText = $row["help"];
+
 $response = <<<EOS
-<div id='popup-help-info'>
 	<div id='help-info-container' class="help-info-container">
 	<span class="help-info-close_btn"  id='help-info-close_btn'>x</span>
-			<textarea cols="80" rows="30" name='help-info-text' id='help-info-text' class='help-info-text'>$helpText</textarea>
 			<input type="button" name="help-info-cancle_btn" id='help-info-cancle_btn' value='Отменить' class='help-info-cancle_btn'>
 			<input type='button' name='help-info-save_btn' id='help-info-save_btn' value='Сохранить' class='help-info-save_btn'>
 			<input type="hidden" id="help-info-id" name="help-info-id" value="$idHelpInfo" />
+			<textarea cols="50" rows="20" name='help-info-text' id='help-info-text' class='help-info-text'>$helpText</textarea>
 	</div>
-</div>
 EOS;
-echo $response;
+	mysql_free_result($result);
+	echo $response;
 }
 if( $type == 'add')
 {
-	//add into base;
+	$helpText = $_POST['text'];//add into base;
+	$strQuery = "update `help_tbl` set `help`='".mysql_escape_string($helpText)."' where `id`=".$idHelpInfo;
+	echo $strQuery;
+	$result = mysql_query($strQuery);
+	if( !$result)
+	{
+		die("не обновилось:".mysql_error());
+	}
+	mysql_free_result($result);
+	echo ' ok';
 }
 ?>
--- js/help_info.js
+++ js/help_info.js
@@ -15,26 +15,30 @@ function showHelpInfo( id )
 function onSuccess(data)
 {
 	document.body.insertAdjacentHTML('beforeEnd',data);
-	$('#popup-help-info').show();
+	$('#help-info-container').show().draggable().resizable();
 
-	var popupHelpInfo = $('#popup-help-info');
-	popupHelpInfo.find('#help-info-close_btn').on('click', onClose)
-	popupHelpInfo.find('#help-info-cancle_btn').on('click', onCancle);
-	popupHelpInfo.find('#help-info-save_btn').on('click', onSave);
+	var HelpInfo = $('#help-info-container');
+	HelpInfo.find('#help-info-close_btn').on('click', onClose)
+	HelpInfo.find('#help-info-cancle_btn').on('click', onCancle);
+	HelpInfo.find('#help-info-save_btn').on('click', onSave);
 	function onCancle(event)
 	{
-		popupHelpInfo.hide();
+		HelpInfo.hide();
 	}
 	function onClose(event)
 	{
-		popupHelpInfo.hide();
+		HelpInfo.hide();
 	}
 
 	function onSave(event)
 	{
-		var idHelpInfo = popupHelpInfo.find('#help-info-id').val();
-		var text = popupHelpInfo.find('#help-info-text').html();
-		$.post("help_info.php",{"id":idHelpInfo,"text":text,"type":'add'});
-		popupHelpInfo.hide();
+		var idHelpInfo = HelpInfo.find('#help-info-id').val();
+		var text = HelpInfo.find('#help-info-text').val();
+		$.post("help_info.php",{"id":idHelpInfo,"text":text,"type":'add'},onSuccessSave,'text');
+		HelpInfo.hide();
+	}
+	function onSuccessSave(data)
+	{
+		console.log(data);
 	}
 }
